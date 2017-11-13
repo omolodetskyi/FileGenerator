3f2f4fe082d0e01e0cb16f65972635714b4d3dff
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2017-11-13 19:44:54 +0200

    initial push, set up pom file, changed test to testng

---
+++ src/main/java/filegenerator/App.java
@@ -0,0 +1,7 @@
+package filegenerator;
+
+public class App {
+	public static void main(String[] args) {
+		// there will be code for filegenerator application
+	}
+}
---
+++ src/test/java/filegenerator/AppTest.java
@@ -0,0 +1,12 @@
+package filegenerator;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	@Test
+	public void testApp() {
+		Assert.assertEquals(true, true, "Just test");
+	}
+}
